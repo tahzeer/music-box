a923c84d57a000c83728cf90bec005b7506403d2
tahzeer <tahzeer10@gmail.com>
2023-09-18 12:02:01 +0530

    db setup

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 
 
-def main(requests):
-    return HttpResponse("<h1> Hello There! <h1>")
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
