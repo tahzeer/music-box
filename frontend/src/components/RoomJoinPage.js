50fdfd48fe640cd333f8d3a2f687dd05c0d39995
tahzeer <tahzeer10@gmail.com>
2023-09-18 16:44:00 +0530

    setup react-router

---
+++ frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RoomJoinPage extends Components {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>This is the Room Join Page</p>;
+  }
+}
